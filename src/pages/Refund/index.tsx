43944b49eea9bdbd25dba5ea954b4cf9b82571ff
yyangLin <yyangmail@qq.com>
2021-07-14 15:25:34 +0800

    退货管理

--- src/pages/Refund/index.tsx
+++ src/pages/Refund/index.tsx
@@ -4,27 +4,33 @@
  * @Author: 王宇阳
  * @Date: 2021-07-14 10:22:13
  * @LastEditors: 王宇阳
- * @LastEditTime: 2021-07-14 11:57:55
+ * @LastEditTime: 2021-07-14 15:24:24
  */
 import SiderMenu from "@/components/SiderMenu";
 import { Row, Col, Button, Divider } from "antd";
 import React, { useEffect, useState } from "react";
-import { deleteFavorite, getFavoriteList } from "./service";
+import { getRefundList } from "./service";
 
-function RefundItem(props: { title: string, price: string, onClick: any, id:number }){
+function RefundItem(props: { title: string, price: string, id:number }){
   return(
     <>
     <div style={{height: '100px',display: 'flex',justifyContent: 'flex-start',alignItems: 'center',width: '100%',}}>
-      <div style={{width:'20%',textAlign:'center'}}>
+      <div style={{width:'40%',textAlign:'center'}}>
         <a href={'/detail?id='+props.id}>
         <span style={{color:'black'}}>{props.title}</span>
         </a>
       </div>
-      <div style={{width:'60%',textAlign:'center'}}>
-        <span>{props.price}</span>
+      <div style={{width:'15%',textAlign:'center'}}>
+        <span>￥{props.price}</span>
       </div>
-      <div style={{width:'20%',textAlign:'center'}}>
-        <span onClick={props.onClick}>删除</span>
+      <div style={{width:'15%',textAlign:'center'}}>
+        <span>2020-08-25 10:12:19</span>
+      </div>
+      <div style={{width:'15%',textAlign:'center',color:'red'}}>
+        <span>未退还</span>
+      </div>
+      <div style={{width:'15%',textAlign:'center',color:'red'}}>
+        <span>处理中</span>
       </div>
     </div>
     <Divider/>
@@ -33,22 +39,14 @@ function RefundItem(props: { title: string, price: string, onClick: any, id:numb
 }
 
 function Refund(props:any){
-  const[favoriteList,setFavoriteList]=useState<Array<{title:string,price:string,id:number}>>([]);
+  const[refundList,setRefundList]=useState<Array<{title:string,price:string,id:number}>>([]);
   useEffect(() => {
-    getFavoriteList().then((res)=>setFavoriteList(res.data));
+    getRefundList().then((res)=>setRefundList(res.data));
   }, [])
-  const deleteHandle=(id:number)=>{
-    deleteFavorite().then((res)=>{
-      if(res.message==='success'){
-        console.log('删除收藏'+id);
-        getFavoriteList().then((res)=>setFavoriteList(res.data));
-      }
-    })
-  }
 
-  const favorites=favoriteList.map((favorite)=>{
+  const refunds=refundList.map((refund)=>{
     return(
-      <RefundItem title={favorite.title} price={favorite.price} onClick={()=>deleteHandle(favorite.id)} id={favorite.id}/>
+      <RefundItem title={refund.title} price={refund.price} id={refund.id}/>
     )
   })
   return(
@@ -58,23 +56,23 @@ function Refund(props:any){
       </Col>
       <Col span={20} offset={2}>
         <div style={{height: '50px',display: 'flex',justifyContent: 'flex-start',alignItems: 'center',backgroundColor: '#C6DCF9',width: '100%',}}>
-          <div style={{width:'30%',textAlign:'center'}}>
+          <div style={{width:'40%',textAlign:'center'}}>
             <span>商品</span>
           </div>
-          <div style={{width:'20%',textAlign:'center'}}>
+          <div style={{width:'15%',textAlign:'center'}}>
             <span>退款金额</span>
           </div>
-          <div style={{width:'30%',textAlign:'center'}}>
+          <div style={{width:'15%',textAlign:'center'}}>
             <span>申请时间</span>
           </div>
-          <div style={{width:'10%',textAlign:'center'}}>
+          <div style={{width:'15%',textAlign:'center'}}>
             <span>退款状态</span>
           </div>
-          <div style={{width:'10%',textAlign:'center'}}>
+          <div style={{width:'15%',textAlign:'center'}}>
             <span>交易操作</span>
           </div>
         </div>
-        {favorites}
+        {refunds}
       </Col>
     </Row>
   )
--- src/pages/Refund/service.ts
+++ src/pages/Refund/service.ts
@@ -4,21 +4,14 @@
  * @Author: 王宇阳
  * @Date: 2021-07-14 10:40:19
  * @LastEditors: 王宇阳
- * @LastEditTime: 2021-07-14 11:13:34
+ * @LastEditTime: 2021-07-14 15:21:08
  */
 import request from 'umi-request';
 
-const getFavoriteListUrl='http://rap2api.taobao.org/app/mock/286636/getFavoriteList'
-const deleteFavoriteUrl='http://rap2api.taobao.org/app/mock/286636/getsuccess'
+const getRefundListUrl='http://rap2api.taobao.org/app/mock/286636/getFavoriteList'
 
-export async function getFavoriteList() {
-  return request(getFavoriteListUrl, {
-    method: 'GET',
-  });
-}
-
-export async function deleteFavorite() {
-  return request(deleteFavoriteUrl, {
+export async function getRefundList() {
+  return request(getRefundListUrl, {
     method: 'GET',
   });
 }
