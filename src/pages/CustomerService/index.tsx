5b07c685f4ab44f6db39d8cf2bd0bb1cf44df39d
kouyou1324 <ian6472@sina.com>
2021-07-15 21:42:17 +0800

    暂存更改

---
+++ src/pages/CustomerService/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SiderMenu from '@/components/SiderMenu';
+import { Col, Row } from 'antd';
+
+const CustomerService: React.FC = () => {
+  return (
+    <Row>
+      <Col>
+        <SiderMenu></SiderMenu>
+      </Col>
+      <Col>
+      
+      </Col>
+    </Row>
+  );
+};
+
+export default CustomerService;
