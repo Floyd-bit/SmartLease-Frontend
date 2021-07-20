3479bcb8442053f42198f73cdd5954fee5bb2a0e
kouyou1324 <ian6472@sina.com>
2021-07-20 16:37:38 +0800

    GetUserId函数位置修改

--- src/components/AddressRow/service.ts
+++ src/components/AddressRow/service.ts
@@ -7,39 +7,65 @@
  * @LastEditTime: 2021-07-20 11:14:40
  */
 import request from 'umi-request';
-import GetUserId from '../GetUserId';
+import GetUserId from '@/utils/GetUserId';
 
-const deleteAddressUrl='/api2/customer/address/deleteById?id='
-const eidtAddressUrl='/api2/customer/address/update?'
-const addAddressUrl='/api2/customer/address/create'
+const deleteAddressUrl = '/api2/customer/address/deleteById?id=';
+const eidtAddressUrl = '/api2/customer/address/update?';
+const addAddressUrl = '/api2/customer/address/create';
 
-export async function deleteAddress(id:number) {
-  return request(deleteAddressUrl+id, {
+export async function deleteAddress(id: number) {
+  return request(deleteAddressUrl + id, {
     method: 'DELETE',
   });
 }
 
-export async function editAddress(address:string,receiverName:string,receiverPhone:string,isDefault:boolean,id:number) {
-  return request(eidtAddressUrl+'address='+address+'&receiverName='+receiverName+'&receiverPhone='+receiverPhone+'&isDefault='+isDefault+'&id='+id+'&userId='+GetUserId(), {
-    method: 'PUT',
-  });
+export async function editAddress(
+  address: string,
+  receiverName: string,
+  receiverPhone: string,
+  isDefault: boolean,
+  id: number,
+) {
+  return request(
+    eidtAddressUrl +
+      'address=' +
+      address +
+      '&receiverName=' +
+      receiverName +
+      '&receiverPhone=' +
+      receiverPhone +
+      '&isDefault=' +
+      isDefault +
+      '&id=' +
+      id +
+      '&userId=' +
+      GetUserId(),
+    {
+      method: 'PUT',
+    },
+  );
 }
 
-export async function addAddress(address:string,receiverName:string,receiverPhone:string,isDefault:boolean) {
+export async function addAddress(
+  address: string,
+  receiverName: string,
+  receiverPhone: string,
+  isDefault: boolean,
+) {
   return request(addAddressUrl, {
     method: 'POST',
-    data:{
+    data: {
       address: address,
-      city: "",
-      country: "",
-      district: "",
+      city: '',
+      country: '',
+      district: '',
       id: 0,
       isDefault: isDefault,
-      province: "",
+      province: '',
       receiverName: receiverName,
       receiverPhone: receiverPhone,
       userId: GetUserId(),
-      zip: ""
-    }
+      zip: '',
+    },
   });
 }
