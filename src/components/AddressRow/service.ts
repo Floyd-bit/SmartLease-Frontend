83d8a5f9bbae3a2cb34bb42870d331e1543521de
yyangLin <yyangmail@qq.com>
2021-07-20 11:15:41 +0800

    收货地址对接

--- src/components/AddressRow/service.ts
+++ src/components/AddressRow/service.ts
@@ -4,28 +4,42 @@
  * @Author: 王宇阳
  * @Date: 2021-07-14 11:20:38
  * @LastEditors: 王宇阳
- * @LastEditTime: 2021-07-14 11:25:57
+ * @LastEditTime: 2021-07-20 11:14:40
  */
 import request from 'umi-request';
+import GetUserId from '../GetUserId';
 
-const deleteAddressUrl='http://rap2api.taobao.org/app/mock/286636/getsuccess'
-const eidtAddressUrl='http://rap2api.taobao.org/app/mock/286636/getsuccess'
-const addAddressUrl='http://rap2api.taobao.org/app/mock/286636/getsuccess'
+const deleteAddressUrl='/api2/customer/address/deleteById?id='
+const eidtAddressUrl='/api2/customer/address/update?'
+const addAddressUrl='/api2/customer/address/create'
 
-export async function deleteAddress() {
-  return request(deleteAddressUrl, {
-    method: 'GET',
+export async function deleteAddress(id:number) {
+  return request(deleteAddressUrl+id, {
+    method: 'DELETE',
   });
 }
 
-export async function editAddress() {
-  return request(eidtAddressUrl, {
-    method: 'GET',
+export async function editAddress(address:string,receiverName:string,receiverPhone:string,isDefault:boolean,id:number) {
+  return request(eidtAddressUrl+'address='+address+'&receiverName='+receiverName+'&receiverPhone='+receiverPhone+'&isDefault='+isDefault+'&id='+id+'&userId='+GetUserId(), {
+    method: 'PUT',
   });
 }
 
-export async function addAddress() {
+export async function addAddress(address:string,receiverName:string,receiverPhone:string,isDefault:boolean) {
   return request(addAddressUrl, {
-    method: 'GET',
+    method: 'POST',
+    data:{
+      address: address,
+      city: "",
+      country: "",
+      district: "",
+      id: 0,
+      isDefault: isDefault,
+      province: "",
+      receiverName: receiverName,
+      receiverPhone: receiverPhone,
+      userId: GetUserId(),
+      zip: ""
+    }
   });
 }
--- src/pages/Address/service.ts
+++ src/pages/Address/service.ts
@@ -4,14 +4,15 @@
  * @Author: 王宇阳
  * @Date: 2021-07-13 15:22:37
  * @LastEditors: 王宇阳
- * @LastEditTime: 2021-07-13 20:02:13
+ * @LastEditTime: 2021-07-20 10:49:12
  */
+import GetUserId from '@/components/GetUserId';
 import request from 'umi-request';
 
-const getAddressListUrl='http://rap2api.taobao.org/app/mock/286636/getAddressList'
+const getAddressListUrl='/api2/customer/address/selectByUserId?userId='
 
 export async function getAddressList() {
-  return request(getAddressListUrl, {
+  return request(getAddressListUrl+GetUserId(), {
     method: 'GET',
   });
 }
