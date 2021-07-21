69fd309083fa1b4992298125b8a19ba8c6b16081
wyf32268886 <1520246115@qq.comxo>
2021-07-21 22:41:51 +0800

    fix: 修复物流、归还

--- src/pages/Transport/index.tsx
+++ src/pages/Transport/index.tsx
@@ -4,15 +4,17 @@
  * @Author: 赵卓轩
  * @Date: 2021-07-21 15:33:53
  * @LastEditors: 赵卓轩
- * @LastEditTime: 2021-07-21 16:49:21
+ * @LastEditTime: 2021-07-21 22:40:23
  */
 import React, {useState,useEffect} from 'react';
-import { Tabs,Row,Col, Table, Tag, Space, Tooltip, Button} from 'antd';
+import { Tabs,Row,Col, Table, Tag, Space, Tooltip, Button, Modal, notification, Input, message} from 'antd';
 import SiderMenu from '@/components/SiderMenu';
 import axios from 'axios';
 import { QuestionCircleOutlined } from '@ant-design/icons';
+import GetUserId from '@/utils/GetUserId';
 
 const { TabPane } = Tabs;
+
 
 const columns = [
   {
@@ -55,15 +57,88 @@ const columns = [
     ],
   },
 ];
-const Transport = () => {
 
+const openNotification = () => {
+  notification.open({
+    message: '温馨提示',
+    description:
+      '在寄出快递后，请务必准确核对您的快递单号。支持中通、圆通、韵达、邮政、顺丰快递。',
+    onClick: () => {
+      console.log('Notification Clicked!');
+    },
+  });
+};
+
+const Transport = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [data,setData] = useState([]);
-  useEffect(async () => {
-    const result = await axios(`/api2/customer/order/getByUserId?pageNum=1&pageSize=5&userId=2`);
-    setData(result.data.data.value.records);
+
+  const backcolumns = [
+    {
+      title: '订单号',
+      width: 120,
+      dataIndex: 'id',
+      render: (_) => <a>{_}</a>,
+    },
+    {
+      title: '状态',
+      width: 120,
+      dataIndex: 'status',
+      render: (_, record) => <Tag color='orange'>待归还</Tag>,
+    },
+    {
+      title: '折扣',
+      width: 120,
+      dataIndex: 'discount',
+      align: 'right',
+    },
+    {
+      title: '模式',
+      width: 120,
+      dataIndex: 'deliveryMode',
+      align: 'right',
+    },
+    {
+      title: '运费',
+      width: 120,
+      dataIndex: 'transportPrice',
+      align: 'right',
+    },
+    {
+      title: '操作',
+      width: 164,
+      key: 'option',
+      valueType: 'option',
+      render: () => [
+        <Button ghost type="primary" size="middle" onClick={()=>{showModal()}}>归还租品</Button>
+      ],
+    },
+  ];
+  
+  const showModal = () => {
+    setIsModalVisible(true);
+    openNotification();
+  };
+
+  const handleOk = () => {
+    message.success('提交成功');
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const result = await axios(`/api2/customer/order/getByUserId?pageNum=1&pageSize=5&userId=${GetUserId()}`);
+      setData(result.data.data.value.records);
+    };
+    fetchData();
   },[]);
 
   return (
+    <>
     <Row justify="space-around" align="top">
     <Col span={2}>
       <SiderMenu selectedKey="14" />
@@ -71,18 +146,20 @@ const Transport = () => {
     <Col span={20} offset={2}>
     <div className="card-container">
     <Tabs type="card">
-      <TabPane tab="已到期租品" key="1">
+      <TabPane tab="已发货租品" key="1">
         <Table columns={columns} dataSource={data} />
       </TabPane>
-      <TabPane tab="已退还租品" key="2">
-        <p>Content of Tab Pane 2</p>
-        <p>Content of Tab Pane 2</p>
-        <p>Content of Tab Pane 2</p>
+      <TabPane tab="待退还租品" key="2">
+      <Table columns={backcolumns} dataSource={data} />
       </TabPane>
     </Tabs>
   </div>
   </Col>
   </Row>
+  <Modal title="填写快递单号" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+      <Input placeholder="请填写快递单号"/>
+  </Modal>
+  </>
   );
 }
 
--- src/pages/TransportDetail/index.tsx
+++ src/pages/TransportDetail/index.tsx
@@ -4,14 +4,25 @@
  * @Author: 赵卓轩
  * @Date: 2021-07-21 16:32:53
  * @LastEditors: 赵卓轩
- * @LastEditTime: 2021-07-21 16:55:12
+ * @LastEditTime: 2021-07-21 22:25:03
  */
 import SiderMenu from '@/components/SiderMenu';
 import { ClockCircleOutlined } from '@ant-design/icons';
 import { Descriptions, Divider, Timeline, Row, Col } from 'antd';
-import React from 'react';
+import React,{useState,useEffect}from 'react';
+import axios from 'axios';
+import GetUserId from '@/utils/GetUserId';
 
 const TransportDetail = () => {
+  const [data,setData] = useState([0]);
+  useEffect(() => {
+    const fetchData = async () => { 
+      const result = await axios(`/api2/customer/address/selectByUserId?userId=${GetUserId()}`);
+      setData((await result).data.data.value);
+    }
+    fetchData();
+    // console.log(result.data.data.value);
+  },[]);
   return (
     <>
     <Row justify="space-around" align="top">
@@ -19,25 +30,30 @@ const TransportDetail = () => {
       <SiderMenu selectedKey="14" />
     </Col>
     <Col span={20} offset={2}>
+      <div>
+        <div>
       <Descriptions title="物流详情">
-        <Descriptions.Item label="收货人姓名">赵卓轩</Descriptions.Item>
-        <Descriptions.Item label="收货人电话">1810000000</Descriptions.Item>
-        <Descriptions.Item label="配送至">Hangzhou, Zhejiang</Descriptions.Item>
-        <Descriptions.Item label="Remark">empty</Descriptions.Item>
+        <Descriptions.Item label="收货人姓名">{data[0].receiverName}</Descriptions.Item>
+        <Descriptions.Item label="收货人电话">{data[0].receiverPhone}</Descriptions.Item>
+        <Descriptions.Item label="国家">{data[0].country}</Descriptions.Item>
+        <Descriptions.Item label="省份">{data[0].province}</Descriptions.Item>
+        <Descriptions.Item label="城市">{data[0].city}</Descriptions.Item>
         <Descriptions.Item label="详细地址">
-          No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+          {data[0].address}
         </Descriptions.Item>
       </Descriptions> 
     <Divider orientation="left">快递动态</Divider>
-      <div style={{marginTop:'200' , marginLeft: '200'}}>
+    </div>
+      <div className="timeline" style={{margin: 100}}>
       <Timeline>
-        <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-        <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
+        <Timeline.Item>已发货  包裹正在等待揽收</Timeline.Item>
         <Timeline.Item dot={<ClockCircleOutlined className="timeline-clock-icon" />} color="red">
-          Technical testing 2015-09-01
+          运输中  已离开 湖北武汉分拨中心；发往 湖北武汉电脑城公司
         </Timeline.Item>
-        <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
+        <Timeline.Item>派送中</Timeline.Item>
+        <Timeline.Item>已签收</Timeline.Item>
       </Timeline>
+      </div>
       </div>
       </Col>
       </Row>
